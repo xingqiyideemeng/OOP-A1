9713e0a17afbe5c04272d353b256f22e9f84c51a
xingqiyideemeng <163309175+xingqiyideemeng@users.noreply.github.com>
2024-11-16 21:34:59 +0800

    First edit HealthProfessional.java

--- First try/HealthProfessional.java
+++ First try/HealthProfessional.java
@@ -1,1 +1,19 @@
+public abstract class HealthProfessional {
+    private String name;
+    private String specialty;
 
+    public HealthProfessional(String name, String specialty) {
+        this.name = name;
+        this.specialty = specialty;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getSpecialty() {
+        return specialty;
+    }
+
+    public abstract void seePatient(String patientName);
+}
