9ce908066af66b8a4df7767a0d04b68d2546f7dd
xingqiyideemeng <163309175+xingqiyideemeng@users.noreply.github.com>
2024-11-19 03:28:46 +0800

    Update GeneralPractitioner.java
    Update and optimize the code to make the code more detailed

--- First try/GeneralPractitioner.java
+++ First try/GeneralPractitioner.java
@@ -1,11 +1,18 @@
 public class GeneralPractitioner extends HealthProfessional {
+    String phoneNumber;
 
-    public GeneralPractitioner(String name) {
-        super(name, "General Practice");
+
+    public GeneralPractitioner() {
     }
 
-    @Override
-    public void seePatient(String patientName) {
-        System.out.println(name + " is seeing patient: " + patientName);
+    public GeneralPractitioner(int ID, String name, String email, String phoneNumber) {
+        this.ID = ID;
+        this.name = name;
+        this.email = email;
+        this.phoneNumber = phoneNumber;
+    }
+    public void printMessage(){
+        super.printMessage();
+        System.out.println("Phone Number: " + phoneNumber);
     }
 }
